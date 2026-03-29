416c74edde59cf12f44d887464c1438e1a2f3c86
Sneha <164773937+SNH078@users.noreply.github.com>
2026-03-29 19:06:54 +0530

    Update Demo1.java

--- 18.Abstract/Demo1.java
+++ 18.Abstract/Demo1.java
@@ -3,7 +3,9 @@
 // 👉 Instantiation is the process of creating an object of a class.
 
 // if there is abstract method then it should be in abstract class ...or the class should be made abstract 
-// its not compulsary to have abstract method in abstrat class ...an abs class can cantain abs method / only non-abs / both abs ,non-abs ...
+// it's not compulsary to have abstract method in abstract class 
+
+//an abs class can contain abs method / only non-abs / both abs ,non-abs ...
 
 abstract class Car { // can't be instantiated
     public abstract void drive(); // abstract method - only function definition
