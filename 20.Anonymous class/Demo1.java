e625e4fab774aa6ef9f44b45f39a371ea267fde3
Sneha <164773937+SNH078@users.noreply.github.com>
2026-03-29 19:55:09 +0530

    Update Demo1.java

--- 20.Anonymous class/Demo1.java
+++ 20.Anonymous class/Demo1.java
@@ -26,7 +26,7 @@ public class Demo1 {
     public static void main(String[] args) {
 
          // Anonymous class
-        //  Anonymous inner class sirf new keyword ke saath hi likhi ja sakti hai, bina kisi existing object ke nahi.
+        //  An anonymous inner class can only be created using the new keyword; it cannot be defined without creating an object
         A obj =new A()
         {  
             public void show()
